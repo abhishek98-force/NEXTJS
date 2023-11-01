c5c11e4e098dea0333d95f6adddbc2fa0364b309
Abhishek <gopalakrishnanunni.a@northeastern.edu>
2023-11-01 19:54:32 -0400

    day 2 commit

---
+++ day_2/app/about/loading.tsx
@@ -0,0 +1,6 @@
+import styles from './style.module.css'
+export default function Loading() {
+  return (
+    <div className={styles.loading}>loading</div>
+  )
+}
---
+++ day_2/app/about/page.tsx
@@ -0,0 +1,13 @@
+import styles from '../page.module.css'
+import Link from 'next/link'
+export default function About() {
+    throw new Error("Not this time!!");
+  return (
+    <>
+    <div>
+        <Link href="/">Link to home page</Link>
+        <h1>This is the about page!!</h1>
+    </div>
+    </>
+  )
+}
